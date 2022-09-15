814aceb1c74db66aed481f8043ccfdb090d92884
AleGla <alegladiali97@gmail.com>
2022-09-15 19:13:39 -0300

    Added log4j2 file to get save all the Gateway redirects and know which endpoint went them

--- GatewayWithJava/src/main/java/com/ar/alegla/configuration/GatewayConfig.java
+++ GatewayWithJava/src/main/java/com/ar/alegla/configuration/GatewayConfig.java
@@ -1,14 +1,24 @@
 package com.ar.alegla.configuration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
 
 
 @Configuration
-public class GatewayConfig {
+public class GatewayConfig implements GlobalFilter{
 
+	private static final Logger log = LoggerFactory.getLogger(GatewayConfig.class);
+	
 	private static final String URI = "http://localhost:8080";
 	private static final String REWRITE_PATH = "/api/service/person";
 	private static final String ALL_PERSONS = "/allPersons";
@@ -38,14 +48,16 @@ public class GatewayConfig {
 				//ALL PERSON SERVICE
 	               .route("allPersons", r -> {
 	                  return r.path(GatewayConfig.REWRITE_PATH + "/all")
-	                        .filters(f -> f
+	                        .filters(f -> f	                        		
 	                        		.addRequestHeader("Content-Type", "application/json")
 	                        		.rewritePath(GatewayConfig.REWRITE_PATH + "/all", GatewayConfig.ALL_PERSONS))
-	                        .uri(GatewayConfig.URI);})
+	                        .uri(GatewayConfig.URI);
+	                        })
 	               
 	           
 	            //FIND PERSON SERVICE
 	               .route("findPerson", r -> {
+	            	   System.out.println("Redirect to " + GatewayConfig.URI + GatewayConfig.FIND_PERSON);
 		                  return r.path(GatewayConfig.REWRITE_PATH + "/find")
 		                        .filters(f -> f
 		                        		.addRequestHeader("Content-Type", "application/json")
@@ -183,4 +195,37 @@ public class GatewayConfig {
 	               .build();
 
     }
+
+
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		// TODO Auto-generated method stub
+		String requestURL =  exchange.getRequest().getURI().getPath().toString();
+		String id = exchange.getRequest().getId();
+		String method = exchange.getRequest().getMethodValue();
+		
+		
+		if (requestURL.equals(GatewayConfig.ALL_PERSONS)) this.saveLog(id, method, requestURL, GatewayConfig.ALL_PERSONS);
+		else if (requestURL.equals(GatewayConfig.FIND_PERSON)) this.saveLog(id, method, requestURL, GatewayConfig.FIND_PERSON);
+		else if (requestURL.equals(GatewayConfig.UPDATE_PERSON)) this.saveLog(id, method, requestURL, GatewayConfig.UPDATE_PERSON);
+		else if (requestURL.equals(GatewayConfig.DELETE_PERSON)) this.saveLog(id, method, requestURL, GatewayConfig.DELETE_PERSON);
+		else if (requestURL.equals(GatewayConfig.FIND_PERSON_BY_NATIONALITY)) this.saveLog(id, method, requestURL, GatewayConfig.FIND_PERSON_BY_NATIONALITY);
+		else if (requestURL.equals(GatewayConfig.FIND_PERSON_BY_NATIONALITY_AND_ORDER_LOW_TO_HIGH_AGE)) this.saveLog(id, method, requestURL, GatewayConfig.FIND_PERSON_BY_NATIONALITY_AND_ORDER_LOW_TO_HIGH_AGE);
+		else if (requestURL.equals(GatewayConfig.FIND_PERSON_BY_NATIONALITY_AND_HIGH_TO_LOW_AGE)) this.saveLog(id, method, requestURL, GatewayConfig.FIND_PERSON);
+		else if (requestURL.equals(GatewayConfig.FIND_PERSON_BY_NATIONALITY_AND_ORDER_LOW_TO_HIGH_MONEY)) this.saveLog(id, method, requestURL, GatewayConfig.FIND_PERSON_BY_NATIONALITY_AND_ORDER_LOW_TO_HIGH_MONEY);
+		else if (requestURL.equals(GatewayConfig.FIND_PERSON_BY_NATIONALITY_AND_ORDER_HIGH_TO_LOW_MONEY)) this.saveLog(id, method, requestURL, GatewayConfig.FIND_PERSON_BY_NATIONALITY_AND_ORDER_HIGH_TO_LOW_MONEY);
+		else if (requestURL.equals(GatewayConfig.ORDER_LOW_TO_HIGH_AGE)) this.saveLog(id, method, requestURL, GatewayConfig.ORDER_LOW_TO_HIGH_AGE);
+		else if (requestURL.equals(GatewayConfig.ORDER_HIGH_TO_LOW_AGE)) this.saveLog(id, method, requestURL, GatewayConfig.ORDER_HIGH_TO_LOW_AGE);
+		else if (requestURL.equals(GatewayConfig.ORDER_LOW_TO_HIGH_MONEY)) this.saveLog(id, method, requestURL, GatewayConfig.ORDER_LOW_TO_HIGH_MONEY);
+		else if (requestURL.equals(GatewayConfig.ORDER_HIGH_TO_LOW_MONEY)) this.saveLog(id, method, requestURL, GatewayConfig.ORDER_LOW_TO_HIGH_MONEY);
+		else if (requestURL.equals(GatewayConfig.FIND_PERSONS_AVAILABLE_TO_TRANSFER)) this.saveLog(id, method, requestURL, GatewayConfig.FIND_PERSONS_AVAILABLE_TO_TRANSFER);
+		else if (requestURL.equals(GatewayConfig.MONEY_TRANSFER)) this.saveLog(id, method, requestURL, GatewayConfig.MONEY_TRANSFER);
+		
+		return chain.filter(exchange);
+	}
+	
+	
+	public void saveLog(String id, String method, String requestURL, String redirectURL) {
+		log.info("{'id' : " + id + ", 'method' : " + method + ", 'Path received' : " + requestURL + ", 'RedirectTo' : " + GatewayConfig.URI + redirectURL + "}");		
+	}
 }
